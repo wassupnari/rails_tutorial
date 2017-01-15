808ad0d0d3081300f87d4e41e4499cb5726c3949
narikim <wassupnari@gmail.com>
2017-01-14 20:19:43 -0800

    List data that created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
+# place the standard CRUD actions in the following order: 
+# index, show, new, edit, create, update and destroy
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
 	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 end
